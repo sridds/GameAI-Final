20e6d096e831726c74b5cf8093d64215338f2e8a
Seth Riddensdale <seth.riddensdale@mymail.champlain.edu>
2025-11-12 19:36:44 -0500

    Training environment setup complete

--- GameAIFinal/Assets/MoveToGoalAgent.cs
+++ GameAIFinal/Assets/MoveToGoalAgent.cs
@@ -7,6 +7,14 @@ public class MoveToGoalAgent : Agent
 {
     [SerializeField]
     private Transform targetTransform;
+    [SerializeField]
+    private float moveSpeed = 2.0f;
+
+    public override void OnEpisodeBegin()
+    {
+        // reset the state for training again
+        transform.position = Vector3.zero;
+    }
 
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -22,7 +30,30 @@ public class MoveToGoalAgent : Agent
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
 
-        float moveSpeed = 1.0f;
         transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
     }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Goal")
+        {
+            SetReward(1f);
+            EndEpisode();
+        }
+
+        if(other.gameObject.tag == "Wall")
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+
+    }
+
 }
