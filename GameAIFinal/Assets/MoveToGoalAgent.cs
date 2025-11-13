d31ab534b92070493e8dc0f9288bcb8ae1542023
Seth Riddensdale <seth.riddensdale@mymail.champlain.edu>
2025-11-12 19:54:40 -0500

    Agents are now trained to move to a target

--- GameAIFinal/Assets/MoveToGoalAgent.cs
+++ GameAIFinal/Assets/MoveToGoalAgent.cs
@@ -9,11 +9,13 @@ public class MoveToGoalAgent : Agent
     private Transform targetTransform;
     [SerializeField]
     private float moveSpeed = 2.0f;
+    [SerializeField]
+    private MeshRenderer planeRenderer;
 
     public override void OnEpisodeBegin()
     {
         // reset the state for training again
-        transform.position = Vector3.zero;
+        transform.localPosition = new Vector3(0, 0.5f, 0);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -21,8 +23,8 @@ public class MoveToGoalAgent : Agent
         // how it observes the environment
         // inputs for the AI
 
-        sensor.AddObservation(transform.position);
-        sensor.AddObservation(targetTransform.position);
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -30,7 +32,7 @@ public class MoveToGoalAgent : Agent
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
 
-        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -45,12 +47,14 @@ public class MoveToGoalAgent : Agent
         if(other.gameObject.tag == "Goal")
         {
             SetReward(1f);
+            planeRenderer.material.color = Color.green;
             EndEpisode();
         }
 
         if(other.gameObject.tag == "Wall")
         {
             SetReward(-1f);
+            planeRenderer.material.color = Color.red;
             EndEpisode();
         }
 
