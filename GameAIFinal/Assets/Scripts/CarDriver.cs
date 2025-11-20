2658efda3287fe937f914871681064deab630107
pricedown <josephmisaacs@gmail.com>
2025-11-20 15:31:38 -0500

    First race track

--- GameAIFinal/Assets/Scripts/CarDriver.cs
+++ GameAIFinal/Assets/Scripts/CarDriver.cs
@@ -53,7 +53,7 @@ public class CarDriver : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody.AddForce(_carOrientation.transform.forward * currentSpeed, ForceMode.Acceleration);
+        _rigidbody.AddForce(-_carOrientation.transform.forward * currentSpeed, ForceMode.Acceleration);
         _rigidbody.AddForce(Vector3.down * _gravity, ForceMode.Acceleration);
         transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotation, 0), Time.deltaTime * 5.0f);
 
